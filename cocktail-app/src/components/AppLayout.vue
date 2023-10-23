f4d967fd5462d569607fd81f6ac39c6a2d968e33
viktor_tumanov <viktor.tumanov@waveaccess.ru>
2023-10-23 13:56:33 +0300

    верстка лейаута

---
+++ cocktail-app/src/components/AppLayout.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="root">
+    <div :style="`background-image: url(${imgUrl})`" class="root__img"></div>
+    <div class="root__main">
+      <el-button class="main__btn">Get random cocktail</el-button>
+    </div>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  imgUrl : {
+    type: String,
+    required: true
+  }
+})
+</script>
+<style lang="scss" scoped>
+  @import '../assets/styles/main.scss';
+
+  .root {
+    display: flex;
+    background-color: $main-bg-c;
+    min-height: 100vh;
+
+    &__img {
+      width: 100%;
+      background-repeat: no-repeat;
+      background-position: 50% 50%;
+      background-size: cover;
+    }
+
+    &__main {
+      width: 50%;
+      position: relative;
+      padding: 32px 40px;
+    }
+  }
+
+  .main__btn {
+    padding: 8px 16px;
+    position: absolute;
+    top: 32px;
+    right: 40px;
+    background-color: $accent;
+    color: $text-c;
+    font-size: 16px;
+    font-weight: 400;
+    border-width: 0;
+    transition: all .2s linear;
+
+    &:hover{
+      background-color: $accent-hov;
+    }
+  }
+</style>
--- cocktail-app/src/pages/Home.vue
+++ cocktail-app/src/pages/Home.vue
@@ -1,7 +1,8 @@
 <template>
-Home
+<AppLayout imgUrl="'/src/assets/img/bg-1.jpg'"/>
 </template>
 <script setup>
+import AppLayout from '../components/AppLayout.vue';
 
 </script>
 <style lang="scss" scoped >
